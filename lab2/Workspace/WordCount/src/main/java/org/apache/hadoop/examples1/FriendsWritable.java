50e8bad2d19aeec800bddea9d3019713f1984849
Mohammed Sayagh <mohammed.sayagh@polymtl.ca>
2015-03-22 10:09:28 -0400

    LAB 2 commits

---
+++ lab2/Workspace/WordCount/src/main/java/org/apache/hadoop/examples1/FriendsWritable.java
@@ -0,0 +1,22 @@
+package org.apache.hadoop.examples1;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Writable;
+
+public class FriendsWritable implements Writable {
+
+	public void write(DataOutput out) throws IOException {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	public void readFields(DataInput in) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
